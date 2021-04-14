6f6a446eb0bcf153c2457197a160050e47b3e122
The Pancilwater <77967318+ThePancilwater@users.noreply.github.com>
2021-04-14 13:59:51 -0500

    insiders commit vs

---
+++ workbench.main.548bbs1985771645vb54563.js
@@ -0,0 +1,12 @@
+function closeAnnouncement() {
+    var a = document.getElementById("announcement");
+    var b = document.getElementById("vs-reader");
+    a.style.display = "none";
+    b.style.display = "block";
+}
+
+function openMobileNavMenuFunction() {
+    var c = document.getElementById("mobileNavMenu")
+    c.style.display = "block";
+}
+
